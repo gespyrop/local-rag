85d37e7e1821bf5bbb409f8c9791edc55b300a22
gespyrop <giwrgosspyropoulos@gmail.com>
2026-02-08 13:38:09 +0100

    feat: ChromaDB integration

--- main.py
+++ main.py
@@ -1,14 +1,85 @@
 '''
 Local RAG
 '''
+import argparse
 from documents import extract_pdf_text, chunk_text
 from embedding import get_text_embeddings
+from vector import ChromaDatabase, VectorQueryResult
+
+
+CHUNK_SIZE = 50
+CHUNK_OVERLAP = 10
+
+
+def add_pdf(pdf_file: str):
+    '''
+    Add a PDF file to the vector database.
+
+    :param pdf_file: PDF file path
+    :type pdf_file: str
+    '''
+    file_name = pdf_file.split('/')[-1].split('.')[0]
+    text = extract_pdf_text(pdf_file)
+
+    chunks = chunk_text(text, CHUNK_SIZE, CHUNK_OVERLAP)
+
+    ids = [f'{file_name}_chunk_{chunk+1}' for chunk in range(len(chunks))]
+    metadatas = [{"source": pdf_file} for _ in chunks]
+    embeddings = get_text_embeddings(chunks)
+
+    db = ChromaDatabase('documents')
+    db.add(ids, embeddings, metadatas, documents=chunks)
+
+
+def search(query: str, k: int = 3) -> list[VectorQueryResult]:
+    '''
+    Search for the `k` most relevant chunks in the database.
+
+    :param query: Search query
+    :type query: str
+    :param k: Number of results
+    :type k: int
+    :return: Dictionary containing the `k` most relevant chunks.
+    :rtype: dict[str, Any]
+    '''
+    embedding = get_text_embeddings(query)
+
+    db = ChromaDatabase('documents')
+    return db.query(embedding, k)
 
 
 if __name__ == "__main__":
-    text = extract_pdf_text('test/test.pdf')
-    chunks = chunk_text(text, 2, 1)
 
-    embedding = get_text_embeddings(chunks)
-    print(len(embedding))
-    print([len(e) for e in embedding])
+    parser = argparse.ArgumentParser(
+        prog='LocalRAG',
+        description='Local retrieval augmented generation'
+    )
+
+    parser.add_argument(
+        '-a', '--add', help='Add a new file to the vector database.')
+    parser.add_argument('-q', '--query', help='Query the vector database.')
+
+    args = parser.parse_args()
+
+    # Add new document
+    if args.add:
+        if not args.add.endswith('.pdf'):
+            print("Unsupported file type. Only PDF files are supported.")
+            exit(1)
+
+        # Add document
+        add_pdf(args.add)
+
+        print(f'Added "{args.add}"')
+
+    # Query
+    elif args.query:
+        print('Query:', args.query)
+
+        for result in search(args.query):
+            print('\n', 10 * '-', f'{result.source}: {result.id}', 10 * '-')
+            print('Chunk:', result.content)
+
+    # Print usage message
+    else:
+        parser.print_help()
---
+++ vector.py
@@ -0,0 +1,95 @@
+'''
+Vector database integrations.
+'''
+from dataclasses import dataclass
+from typing import Any, Protocol, Sequence, Mapping
+
+import chromadb
+
+
+@dataclass
+class VectorQueryResult:
+    '''
+    Represents a single query result
+    '''
+    id: str
+    source: str
+    content: str
+    distance: float
+
+
+class VectorDatabase(Protocol):
+    '''
+    Abstract Vector Database
+    '''
+
+    def add(
+        self,
+        ids: Sequence[str],
+        embeddings: Sequence[Sequence[float]],
+        metadatas: Sequence[Mapping[str, Any]],
+        documents: Sequence[str],
+    ) -> None:
+        '''
+        Add documents to the database.
+
+        :param ids: Document identifiers
+        :type ids: Sequence[str]
+        :param embeddings: Document embeddings
+        :type embeddings: Sequence[Sequence[float]]
+        :param documents: Document contents
+        :type documents: Sequence[str]
+        :param metadatas: Document metadata
+        :type metadatas: Sequence[Mapping[str, Any]]
+        '''
+        ...
+
+    def query(self,
+              embedding: Sequence[float],
+              k: int = 3) -> list[VectorQueryResult]:
+        '''
+        Query documents from the database.
+
+        :param embedding: Query embedding
+        :type embedding: Sequence[float]
+        :param k: Number of results
+        :type k: int
+        :return: k most similar documents
+        :rtype: list[VectorQueryResult]
+        '''
+        ...
+
+
+class ChromaDatabase(VectorDatabase):
+    '''
+    Chroma Vector Database
+    '''
+
+    def __init__(self, collection: str):
+        self.client = chromadb.PersistentClient()
+        self.collection = self.client.get_or_create_collection(collection)
+
+    def add(self, ids, embeddings, metadatas, documents):
+        self.collection.add(ids, embeddings, metadatas, documents)
+
+    def query(self, embedding, k=3):
+        # Query result
+        qr = self.collection.query(query_embeddings=[embedding], n_results=k)
+
+        # Number of returned results
+        num_results = len(qr['ids'][0])
+
+        # Final results
+        results: list[VectorQueryResult] = []
+
+        for index in range(num_results):
+            results.append(
+                VectorQueryResult(
+                    qr['ids'][0][index],
+                    qr['metadatas'][0][index]['source'],
+                    qr['documents'][0][index],
+                    qr['distances'][0][index]
+                )
+            )
+
+        return results
